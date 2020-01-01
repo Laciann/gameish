8a18805b8731515251b90ef3fac8f709178dc849
Laciann <abhishek.acharya@apexcollege.edu.np>
2020-01-01 14:00:17 +0545

    logic for draw

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,10 +13,12 @@ function attackHandler(chosenMaxLife) {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
 
-    if(currentMonsterHealth<=0){
+    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert('You won');
-    } else if(currentPlayerHealth<=0){
+    } else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
         alert('Monster won');
+    } else if(currentPlayerHealth<=0 && currentMonsterHealth<=0){
+        alert('You have a draw');
     }
 }
 
